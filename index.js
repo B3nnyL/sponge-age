7b1332820f4cdd1250d91985ae31cda03fd75a20
b3nnyL <shuyinglin47@gmail.com>
2019-03-13 12:54:43 +1100

    get styled

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const app = express();
 
 const id = "zdIGTIdD1mi4";
 const url = `https://media.giphy.com/media/${id}/giphy.gif`;
-
+const pageTitle = `Sponge Time`;
 app.get("*", (req, res) => {
-  res.write(`<body><img src="${url}"></body>`);
+  res.write(
+    `<head>
+      <meta name="viewport" content="width=device-width, minimum-scale=0.1">
+      <meta property="og:title" content="${pageTitle}" />
+      <meta property="og:image" content="${url}" />
+      <title>${pageTitle}</title>
+      </head>`
+  );
+  res.write(
+    `<body style="margin: 0px; background: #0e0e0e;">
+    <img src="${url}"></body>`
+  );
   res.end();
 });
 
