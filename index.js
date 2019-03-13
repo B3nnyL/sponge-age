af5f2957cb4dd33b66d2ef3f99caca8c0402f4b7
b3nnyL <shuyinglin47@gmail.com>
2019-03-13 13:48:07 +1100

    refactor

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 const express = require("express");
-const fetch = require("node-fetch");
+const id = require("./api");
 const app = express();
 
-const API_KEY = "3ABkTp4vp1uBEi2PJxYEIezmWEVwfzff";
-
-async function id() {
-  const promise = await fetch(
-    `http://api.giphy.com/v1/gifs/random?tag=spongeBob&api_key=${API_KEY}&limit=1"`
-  );
-  let result = await promise.json();
-  console.log(result.data);
-  return result.data.id;
-}
-
-const _id = id()
-  .then(data => data)
-  .catch(err => console.error(err));
-const url = `https://media.giphy.com/media/${_id}/giphy.gif`;
-const downsized_url = `https://media.giphy.com/media/${_id}/200w_d.gif`;
-const pageTitle = `Sponge Time`;
-app.get("*", (req, res) => {
+app.get("*", async (req, res) => {
+  const pageTitle = `Sponge Time`;
+  const _id = await id();
+  const url = `https://media.giphy.com/media/${_id}/giphy.gif`;
+  const downsized_url = `https://media.giphy.com/media/${_id}/200w_d.gif`;
   res.write(
     `<head>
       <meta name="viewport" content="width=device-width, minimum-scale=0.1">
