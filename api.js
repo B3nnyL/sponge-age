4a84e6cfa8b82316a1805bc8ad31ebd50a59887c
b3nnyL <shuyinglin47@gmail.com>
2019-03-13 14:00:07 +1100

    get api key from env variable

--- api.js
+++ api.js
@@ -1,9 +1,10 @@
-const API_KEY = "3ABkTp4vp1uBEi2PJxYEIezmWEVwfzff";
 const fetch = require("node-fetch");
 
 module.exports = async function id() {
   const promise = await fetch(
-    `http://api.giphy.com/v1/gifs/random?tag=spongeBob&api_key=${API_KEY}&limit=1"`
+    `http://api.giphy.com/v1/gifs/random?tag=spongeBob&api_key=${
+      process.env.API_KEY
+    }&limit=1"`
   );
   let result = await promise.json();
   return result.data.id;
