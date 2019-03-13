c710f18114a977f390a5bc0a6c93dada4de3b68c
b3nnyL <shuyinglin47@gmail.com>
2019-03-13 14:15:20 +1100

    use more accurate api

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 
 module.exports = async function id() {
   const promise = await fetch(
-    `http://api.giphy.com/v1/gifs/random?tag=spongeBob&api_key=${
+    `http://api.giphy.com/v1/gifs/random?tag=spongebob-squarepants&api_key=${
       process.env.API_KEY
     }&limit=1"`
   );
